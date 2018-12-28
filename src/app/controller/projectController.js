8ebffb26d3b5af01fb429329a6c429b65dfe25ab
GabrielLinss <glins731@gmail.com>
2018-12-28 13:19:45 -0300

    add routes to project functions controller

--- src/app/controller/projectController.js
+++ src/app/controller/projectController.js
@@ -1,3 +1,34 @@
-exports.base = async (req, res) => {
-    res.send({ok: true, user: req.userId});
+const Project = require('../model/project');
+const Task = require('../model/task');
+
+exports.listAll = async (req, res) => {
+    try{
+        const projects = await Project.find();
+
+        return res.send({projects});
+    } catch(err){
+        res.status(400).send({error: 'Error loading projects'});
+    }
+};
+
+exports.find = async (req, res) => {
+    res.send({user: req.userId});
+};
+
+exports.save = async (req, res) => {
+    try{
+        const project = await Project.create({...req.body, user: req.userId});
+
+        return res.send({project});
+    } catch(err){
+        res.status(400).send({error: 'Error creating new project'});
+    }
+};
+
+exports.update = async (req, res) => {
+    res.send({user: req.userId});
+};
+
+exports.delete = async (req, res) => {
+    res.send({user: req.userId});
 };
--- src/routes/projectRoutes.js
+++ src/routes/projectRoutes.js
@@ -6,7 +6,23 @@ const authMiddleware = require('../app/middlewares/auth');
 router.use(authMiddleware);
 
 router.get('/', async (req, res) => {
-    controller.base(req, res);
+    controller.listAll(req, res);
+});
+
+router.get('/:projectId', async (req, res) => {
+    controller.find(req, res);
+});
+
+router.post('/', async (req, res) => {
+    controller.save(req, res);
+});
+
+router.put('/:projectId', async (req, res) => {
+    controller.update(req, res);
+});
+
+router.delete('/:projectId', async (req, res) => {
+    controller.delete(req, res);
 });
 
 module.exports = app => app.use('/projects', router);
