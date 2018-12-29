292baf1295e2b011c3a9bbfbcf5237a5577da7d6
GabrielLinss <glins731@gmail.com>
2018-12-29 13:12:39 -0300

    modified projectController to operations CRUD

--- src/app/controller/projectController.js
+++ src/app/controller/projectController.js
@@ -3,7 +3,7 @@ const Task = require('../model/task');
 
 exports.listAll = async (req, res) => {
     try{
-        const projects = await Project.find();
+        const projects = await Project.find().populate(['user', 'tasks']);
 
         return res.send({projects});
     } catch(err){
@@ -12,12 +12,29 @@ exports.listAll = async (req, res) => {
 };
 
 exports.find = async (req, res) => {
-    res.send({user: req.userId});
+    try{
+        const project = await Project.findById(req.params.projectId).populate(['user', 'tasks']);
+
+        return res.send({project});
+    } catch(err){
+        res.status(400).send({error: 'Error loading project'});
+    }
 };
 
 exports.save = async (req, res) => {
     try{
-        const project = await Project.create({...req.body, user: req.userId});
+        const {title, description, tasks} = req.body;
+
+        const project = await Project.create({title, description, user: req.userId});
+
+        await Promise.all(tasks.map(async task => {
+            const projectTask = new Task({...task, project: project._id});
+            await projectTask.save();
+
+            project.tasks.push(projectTask);
+        }));
+
+        await project.save();
 
         return res.send({project});
     } catch(err){
@@ -26,9 +43,35 @@ exports.save = async (req, res) => {
 };
 
 exports.update = async (req, res) => {
-    res.send({user: req.userId});
+    try{
+        const {title, description, tasks} = req.body;
+
+        const project = await Project.findByIdAndUpdate(req.params.projectId, {title, description}, {new: true});
+
+        project.tasks = [];
+        await Task.remove({project: project._id});
+
+        await Promise.all(tasks.map(async task => {
+            const projectTask = new Task({...task, project: project._id});
+            await projectTask.save();
+
+            project.tasks.push(projectTask);
+        }));
+
+        await project.save();
+
+        return res.send({project});
+    } catch(err){
+        res.status(400).send({error: 'Error updating new project'});
+    }
 };
 
 exports.delete = async (req, res) => {
-    res.send({user: req.userId});
+    try{
+        await Project.findByIdAndRemove(req.params.projectId);
+
+        return res.send();
+    } catch(err){
+        res.status(400).send({error: 'Error removing project'});
+    }
 };
