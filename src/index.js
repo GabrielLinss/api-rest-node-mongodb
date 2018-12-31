d0d473cbcd9c5208d593cac967addedcd643a3a4
GabrielLinss <glins731@gmail.com>
2018-12-30 23:26:49 -0300

    enable config CORS to express

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
 
 require('./routes/routes')(app);
 require('./routes/projectRoutes')(app);
 
 app.listen(8000);
+console.log('Listening on port 8000');
